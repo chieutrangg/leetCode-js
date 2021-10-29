47b4bf75b96b7dc79b43219aca76d3db22cedaca
chieutrangg <chieutrangg@gmail.com>
2021-10-29 13:44:36 +0300

    palindome number

--- index.js
+++ index.js
@@ -14,3 +14,27 @@
         }
     }
 };
+
+/*Palindrome number*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+ let isPalindrome = function(x) {
+    x = x.toString();
+    let i = 0;
+    let j = x.length - 1;
+    let result = true;
+ 
+    while ( i < j) {
+        if (x[i] != x[j]) {
+            result = false;
+            break;
+        } 
+        i++;
+        j--;
+    }
+ 
+    return result;
+ };

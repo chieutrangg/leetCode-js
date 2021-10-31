f1666df97b9a0e9962eff47c5e2053b8c7f28d6b
chieutrangg <chieutrangg@gmail.com>
2021-10-31 21:08:13 +0200

    leetCode roman to integer

--- index.js
+++ index.js
@@ -38,3 +38,17 @@
  
     return result;
  };
+
+/*Roman to integer*/  
+ var romanToInt = function(s) {
+    let arr = s.split("");
+    let roman = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    let prev = Infinity;
+    let result = 0; //DCXXI
+    for (let i of arr) {
+        if (roman[i]>prev) result-=prev*2
+        result+=roman[i];
+        prev = roman[i];
+    }
+    return result
+};

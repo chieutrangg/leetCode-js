00daa2d380b63eda27c278682c97fe1179597d7a
chieutrangg <chieutrangg@gmail.com>
2021-11-04 18:26:46 +0200

    adding longest common prefix

--- index.js
+++ index.js
@@ -52,3 +52,16 @@
     }
     return result
 };
+
+/*Longest common prefix*/
+var longestCommonPrefix = function(strs) {
+    
+    let prefix = ""
+  
+    for(let i = 0; i < strs[0].length; i++){
+        if(strs.every(e => e[i] === strs[0][i])){
+            prefix += strs[0][i]
+        }else return prefix
+    }
+    return prefix
+}; 
